113ab1760a5eb1f8ba7b58b7ecf915aeabb32bc5
sunnyaxin <137126846@qq.com>
2020-03-03 20:15:15 +0800

    Extract method in getScore for tennis game1 - Large Method.

--- src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
@@ -26,49 +26,74 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
+        if (hasSameScore()) {
+            score = printSameScore();
+        } else if (hasLargeScoreGap()) {
+            score = printLargeScore();
+        } else {
+            score = printSmallScore(score);
+        }
+        return score;
+    }
+
+    private boolean hasSameScore() {
+        return score1 == score2;
+    }
+
+    private boolean hasLargeScoreGap() {
+        return score1 >= 4 || score2 >= 4;
+    }
+
+    private String printSameScore() {
+        String score;
+        switch (score1) {
+            case 0:
+                score = LOVE + ALL;
+                break;
+            case 1:
+                score = FIFTEEN + ALL;
+                break;
+            case 2:
+                score = THIRTY + ALL;
+                break;
+            default:
+                score = DEUCE;
+                break;
+        }
+        return score;
+    }
+
+    private String printLargeScore() {
+        String score;
+        int minusResult = score1 - score2;
+        if (minusResult == 1) score = ADVANTAGE + PLAYER_ONE;
+        else if (minusResult == -1) score = ADVANTAGE + PLAYER_TWO;
+        else if (minusResult >= 2) score = WIN_FOR + PLAYER_ONE;
+        else score = WIN_FOR + PLAYER_TWO;
+        return score;
+    }
+
+    private String printSmallScore(String score) {
         int tempScore;
-        if (score1 == score2) {
-            switch (score1) {
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = score1;
+            else {
+                score += DIVIDER;
+                tempScore = score2;
+            }
+            switch (tempScore) {
                 case 0:
-                    score = LOVE + ALL;
+                    score += LOVE;
                     break;
                 case 1:
-                    score = FIFTEEN + ALL;
+                    score += FIFTEEN;
                     break;
                 case 2:
-                    score = THIRTY + ALL;
+                    score += THIRTY;
                     break;
-                default:
-                    score = DEUCE;
+                case 3:
+                    score += FORTY;
                     break;
-            }
-        } else if (score1 >= 4 || score2 >= 4) {
-            int minusResult = score1 - score2;
-            if (minusResult == 1) score = ADVANTAGE + PLAYER_ONE;
-            else if (minusResult == -1) score = ADVANTAGE + PLAYER_TWO;
-            else if (minusResult >= 2) score = WIN_FOR + PLAYER_ONE;
-            else score = WIN_FOR + PLAYER_TWO;
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = score1;
-                else {
-                    score += DIVIDER;
-                    tempScore = score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += LOVE;
-                        break;
-                    case 1:
-                        score += FIFTEEN;
-                        break;
-                    case 2:
-                        score += THIRTY;
-                        break;
-                    case 3:
-                        score += FORTY;
-                        break;
-                }
             }
         }
         return score;
